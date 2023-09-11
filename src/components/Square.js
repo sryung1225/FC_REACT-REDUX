103c2e4366a439b639f16194938532e3240a5712
sryung <sryung1225@gmail.com>
2023-09-11 15:54:36 +0900

    [🥁] 컴포넌트 기초 공사

---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Square.css";
+
+class Square extends Component {
+  render() {
+    return <button className="square">Square</button>;
+  }
+}
+
+export default Square;
