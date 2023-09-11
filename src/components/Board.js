8dcd691aafed47d4841c865fb8ca3202f5f8b749
sryung <sryung1225@gmail.com>
2023-09-11 18:11:35 +0900

    [🥁] 승부 결정하기 - 3줄 완성 승리 조건 완성 : calculateWinner() - 중복 sqaure 클릭 & 승부 결정 시 다음 동작하지 않도록 방어

--- src/components/Board.js
+++ src/components/Board.js
@@ -5,8 +5,43 @@ import Square from "./Square";
 const Board = () => {
   const [squares, setSqaures] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ` + winner;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+
   const handleClick = (i) => {
     const newSquares = squares.slice(); // 원본 배열 손상없이 수정하기 위해 slice()로 복제
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
     newSquares[i] = xIsNext ? "X" : "O";
     setSqaures(newSquares);
     setXIsNext((prev) => !prev);
@@ -14,9 +49,10 @@ const Board = () => {
   const renderSquare = (i) => {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   };
+
   return (
     <div>
-      <div className="status">Next Player: {xIsNext ? "X" : "O"}</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
