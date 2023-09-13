4e9833aef05ed785d3f93067b8cf277425c3a107
sryung <sryung1225@gmail.com>
2023-09-13 16:59:57 +0900

    [🥁] 경기 진행 기록 저장하기 - 경기 진행 기록 history 와 가장 최근 동작 current - 필요한 데이터를 Board 컴포넌트 바깥으로 꺼내 최상위 단계인 App으로 이동. Board에는 props 전달

--- src/App.js
+++ src/App.js
@@ -1,13 +1,60 @@
+import { useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
 
 function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const current = history[history.length - 1];
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+  const winner = calculateWinner(current.squares);
+  let status;
+  if (winner) {
+    status = `Winner: ` + winner;
+  } else {
+    status = `Next player: ${xIsNext ? "X" : "O"}`;
+  }
+  const handleClick = (i) => {
+    const newSquares = current.squares.slice(); // 원본 배열 손상없이 수정하기 위해 slice()로 복제
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = xIsNext ? "X" : "O";
+    setHistory([...history, { squares: newSquares }]);
+    setXIsNext((prev) => !prev);
+  };
+  console.log(history);
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} handleClick={handleClick} />
       </div>
-      <div className="game-info">game-info</div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+      </div>
     </div>
   );
 }
